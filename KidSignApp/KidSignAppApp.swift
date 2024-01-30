19ed7c63b61f874ffa68296369525941b45b4cee
amalalreshi <amal.alreshi@gmail.com>
2024-01-30 11:02:36 +0300

    Initial Commit

---
+++ KidSignApp/KidSignAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  KidSignAppApp.swift
+//  KidSignApp
+//
+//  Created by Amal Mohammad Alreshi on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct KidSignAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
